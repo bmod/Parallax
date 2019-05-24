e8169e5ccb03c9fcb59c38c3856f98355f3e5096
bmod <bmodone@gmail.com>
2019-05-23 21:10:55 -0700

    tweaks

--- Assets/ParallaxLayer.cs
+++ Assets/ParallaxLayer.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ParallaxLayer : MonoBehaviour {
-   public float fraction = 0.0f;
+   public float fraction;
    
    void UpdatePosition() {
       var camPos = Camera.main.transform.position;
