845f5fde88236a8f9a86e935ca22a688d54d92c4
bmod <bmodone@gmail.com>
2019-05-23 21:05:16 -0700

    Initial commit

---
+++ Assets/ParallaxLayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour {
+   public float fraction = 0.0f;
+   
+   void UpdatePosition() {
+      var camPos = Camera.main.transform.position;
+      var pos = new Vector3(camPos.x * fraction, camPos.y * fraction, transform.position.z);
+      transform.position = pos;
+   }
+
+   private void OnDrawGizmos() {
+       UpdatePosition();
+   }
+}
